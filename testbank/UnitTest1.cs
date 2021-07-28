9dcfcb7e25576afab9035d333088ae2314a3f60d
nehann123 <nalageneha99@gmail.com>
2021-07-28 16:29:47 +0530

    ref

---
+++ testbank/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace testbank
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
